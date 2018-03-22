9ed4d6db3713b00d67e9998e16d899b204443be0
Elzoka <mahmoud_elzoka@yahoo.com>
2018-03-22 16:46:24 +0200

    add database and read routes

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-let campgrounds = [
-    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1468956398224-6d6f66e22c35?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5d2e4d45d037053be722233b79bd0510&auto=format&fit=crop&w=1955&q=80"},
-    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d1c8cc988efddbda8746281871c0c8bf&auto=format&fit=crop&w=1259&q=80"},
-    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1432817495152-77aa949fb1e2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a6f210acc36ab5742aa863e7a2240a2a&auto=format&fit=crop&w=1349&q=80"}
-];
-
 // config
 require('./config/config');
+
+// require third party modules
+const express    = require('express');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+const app        = express();
+
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Granite Hill",
+//     image: "https://images.unsplash.com/photo-1493235431945-90c060301e41?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0279978342b1a6b4f3e0e00a0784c2e3&auto=format&fit=crop&w=1350&q=80",
+//     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+// })
+//     .then(campground => {
+//
+//     });
+
+mongoose.connect(process.env.MONGODB_URI);
+
 
 //bodyParser middleware
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,7 +39,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {campgrounds});
+    Campground.find({})
+        .then(campgrounds => {
+            res.render('index', {campgrounds});
+        })
+        .catch(err => {
+            console.log(err);
+        })
 });
 
 app.get('/campgrounds/new', (req, res) =>{
@@ -30,12 +53,24 @@ app.get('/campgrounds/new', (req, res) =>{
 });
 
 app.post('/campgrounds', (req, res) => {
-    const newCampground = {
+    Campground.create({
         name: req.body.name,
-        image: req.body.image
-    }
-    campgrounds.push(newCampground);
-    res.redirect('/campgrounds');
+        image: req.body.image,
+        description: req.body.description
+    })
+        .then(campground => {
+            res.redirect('/campgrounds');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+app.get('/campgrounds/:id', (req, res) => {
+    Campground.findById(req.params.id)
+        .then(campground => {
+            res.render('show', {campground});
+        })
+        .catch(err => console.log(err));
 });
 
 app.listen(process.env.PORT, () => {
--- config/config.js
+++ config/config.js
@@ -2,4 +2,5 @@ process.env.NODE_ENV = process.env.NODE_ENV|| 'developement';
 
 if(process.env.NODE_ENV === 'developement'){
     process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost/yelpcamp';
 }
