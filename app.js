45e0329a188982af8e596379afe914a7b7d76070
Elzoka <mahmoud_elzoka@yahoo.com>
2018-03-22 18:54:34 +0200

    Add comment routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app        = express();
 
 // require models
 const Campground = require('./models/campground');
+const Comment = require('./models/comment');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -30,7 +31,7 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', (req, res) => {
     Campground.find({})
         .then(campgrounds => {
-            res.render('index', {campgrounds});
+            res.render('campgrounds/index', {campgrounds});
         })
         .catch(err => {
             console.log(err);
@@ -38,7 +39,7 @@ app.get('/campgrounds', (req, res) => {
 });
 
 app.get('/campgrounds/new', (req, res) =>{
-    res.render('new.ejs');
+    res.render('campgrounds/new');
 });
 
 app.post('/campgrounds', (req, res) => {
@@ -58,9 +59,42 @@ app.get('/campgrounds/:id', (req, res) => {
     Campground.findById(req.params.id)
         .populate('comments')
         .then(campground => {
-            res.render('show', {campground});
+            res.render('campgrounds/show', {campground});
         })
         .catch(err => console.log(err));
+});
+
+// =======================
+// COMMENTS ROUTES
+// =======================
+
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+    Campground.findById(req.params.id)
+        .then(campground => {
+            res.render('comments/new', {campground});
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+    Campground.findById(req.params.id)
+        .then(campground => {
+            Comment.create(req.body.comment)
+                .then(comment => {
+                    campground.comments.push(comment);
+                    campground.save()
+                    res.redirect(`/campgrounds/${campground._id}`);
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect('/campgrounds');
+        })
 });
 
 app.listen(process.env.PORT, () => {
